caed81dde60f08f07a095348d860ab5f74fbcd50
COBIECODES <dibia3527@gmail.com>
2023-07-06 09:38:35 -0700

    pack again

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,7 @@
 import os
 from fabric.api import *
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.237.32.213', '34.232.53.51']
 
 
 def do_clean(number=0):
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,9 +6,9 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.237.32.213', '34.232.53.51']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
